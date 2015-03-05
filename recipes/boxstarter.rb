789e9d5032578ec9a7e5189dbd0ea6900c01d2b6
Tom Hensel <github@jitter.eu>
2015-03-05 21:08:10 +0100

    simplified

--- recipes/boxstarter.rb
+++ recipes/boxstarter.rb
@@ -4,18 +4,9 @@ config = node['squeegee-run']['config']
 include_recipe 'boxstarter::default'
 
 boxstarter 'boxstarter run' do
-  disable_reboots false
+  disable_reboots true
   code <<-EOH
     Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-    # Enable-RemoteDesktop
-    cinst chocolateygui
-    cinst sysinternals
-    cinst procexp
     cinst cygwin
-    cinst atom
-    cinst console2
-    cinst poshgit
-    cinst duck
-    cinst putty
   EOH
 end
