e25ebec8af3b92234591197ce9355bc64a66b928
Tom Hensel <github@jitter.eu>
2015-03-05 02:57:05 +0100

    updated readme bump

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 squeegee-run
 ============
+
+Chef cookbook to setup a Windows system using [squeegee](https://github.com/gretel/squeegee). Wraps around [Boxstarter](http://boxstarter.org/) nice and easy.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tom@jitter.eu'
 license          'BSD 3-Clause License'
 description      'Cookbook used on the initial Chef run of a instance provisioned using ''squeegee'''
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 # https://github.com/mwrock/boxstarter-cookbook
 depends 'boxstarter'
